a58a347fe14175326508c9e6a20eab756c3d6129
maomin <maomin@163.com>
2018-07-04 09:26:28 +0800

    version 2.0

--- application/common.php
+++ application/common.php
@@ -1,9 +1,4 @@
 <?php
-// +----------------------------------------------------------------------
-// | laychat-v3.0
-// +----------------------------------------------------------------------
-// | Author: NickBai <1902822973@qq.com>
-// +----------------------------------------------------------------------
 /**
  * 写基础配置文件
  * @param $data
@@ -45,31 +40,5 @@ function readCtConfig()
     return unserialize($conf);
 }
 
-//获取评论
-function getComment($blogId)
-{
-    $list = db('comment')->where('blog_id', $blogId)->select();
-    if(empty($list)){
 
-        echo "";
-    }else{
-
-        $html = '';
-        foreach($list as $key=>$vo){
-            $html .= '<a href="javascript:;" class="pull-left"><img alt="image" src="' . $vo['com_avatar'] . '"></a>';
-            $html .= '<div class="media-body"><a href="javascript:;" style="color:#337AB7">' . $vo['com_user'];
-            $html .= '&nbsp;&nbsp;&nbsp;&nbsp;</a>' . $vo['content'] . '<br/>';
-            $html .= '<small class="text-muted">' . date('Y-m-d H:i', $vo['com_time']) . '</small></div>';
-        }
-
-        echo $html;
-    }
-
-}
-
-//将对象转换成数组
-function objToArr($obj)
-{
-    return json_decode(json_encode($obj), true)['data'];
-}
 
